371010809ead69e612b06b74af4737a99752fdcf
DorCarmi <dorc@post.bgu.ac.il>
2020-04-14 13:33:08 +0300

    finish contacInfo test

---
+++ dev/Tests/ContactInfoTest.java
@@ -0,0 +1,37 @@
+package Tests;
+
+import Supplier.ContactInfo;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactInfoTest {
+    private ContactInfo contactInfo;
+
+    @BeforeEach
+    void setUp() {
+        contactInfo=new ContactInfo("David","054","@com");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPhoneNumber() {
+        Assert.assertEquals(contactInfo.getPhoneNumber(),"054");
+    }
+
+    @Test
+    void getEmail() {
+        Assert.assertEquals(contactInfo.getEmail(),"@com");
+    }
+
+    @Test
+    void getName() {
+        Assert.assertEquals(contactInfo.getName(),"David");
+    }
+}
