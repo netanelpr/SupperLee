74010e82038e8099dd64aeb24993e3e22b3902b6
netanelpr <pratn@post.bgu.ac.il>
2020-05-04 19:41:50 +0300

    Add db handler
    Add db hdndler

--- dev/Main.java
+++ dev/Main.java
@@ -1,5 +1,17 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:DB/example.db")){
+            System.out.println("Open DB");
+
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM Product WHERE barcode = 1");
+            ResultSet res = pstmt.executeQuery();
+
+            System.out.println("barcode: " + res.getInt("barcode") + " name: " + res.getString("name"));
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
