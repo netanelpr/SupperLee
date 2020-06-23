9d043fd45b7f4755e7c7d48f5cdb0bcef52cebe4
Andrey <palmana@post.bgu.ac.il>
2020-06-23 22:29:58 +0300

    no message

---
+++ src/test/java/Trans_HR/Business_Layer/Workers/Controllers/WorkerControllerTest.java
@@ -0,0 +1,19 @@
+package Trans_HR.Business_Layer.Workers.Controllers;
+
+import junit.framework.TestCase;
+
+public class WorkerControllerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetAllDrivers() {
+    }
+
+    public void testAddDriver() {
+    }
+}
